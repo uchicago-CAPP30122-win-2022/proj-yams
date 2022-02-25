c4377b654e0236ed12769a202f67e113a2374a17
marcdloeb <marcdloeb@gmail.com>
2022-02-25 17:29:36 -0600

    permit_work

--- comm_areas.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sodapy import Socrata
-
-client = Socrata("data.cityofchicago.org", "goD601SLndI51xcMq1KsnG6np")
-ca_results = client.get("igwz-8jzy")
-comm_area = pd.DataFrame.from_records(ca_results)
