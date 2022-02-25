4f3b3143591177847a54dd96698e4304ffdf172c
marcdloeb <marcdloeb@gmail.com>
2022-02-25 16:02:52 -0600

    updates

--- api_collection.py
+++ api_collection.py
@@ -1,16 +1,13 @@
 import pandas as pd
 from sodapy import Socrata
 
-id_dict = {"red light violations": "spqx-js37", "abandoned buildings": "kc9i-wq85", \
-    "socioeconomic indicators": "kn9c-c2s2", "hardship index": "792q-4jtu", \
-        "crimes": "ijzp-q8t2", "grocery stores": "4u6w-irs9", }
+id_dict = {"red_light_violations": "spqx-js37", "abandoned_buildings": "kc9i-wq85", \
+    "socioeconomic_indicators": "kn9c-c2s2", "hardship_index": "792q-4jtu", \
+        "crimes": "ijzp-q8t2", "grocery_stores": "4u6w-irs9", }
 
 # Unauthenticated client only works with public data sets. Note 'None'
 # in place of application token, and no username or password:
 
-def pull_data(dataset_id, limit=None):
-    client = Socrata("data.cityofchicago.org", None)
-
-    #enter dataset identifier and limit for number of rows.
-    results = client.get(dataset_identifier, limit)
-    return pd.DataFrame.from_records(results)
+def create_dataframes: 
+    for key, value in id_dict.items():
+        key = chicago_open_api(value):
