fde0394026a35ed25ec0a08a43ef3401f9ad8f45
marcdloeb <marcdloeb@gmail.com>
2022-02-25 15:50:07 -0600

    api:

--- comm_areas.py
+++ comm_areas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sodapy import Socrata
 
-client = Socrata("data.cityofchicago.org", "goD601SLndI51xcMq1KsnG6np", username = "marcdloeb@gmail.com", password = "2522-Haphazard")
+client = Socrata("data.cityofchicago.org", "goD601SLndI51xcMq1KsnG6np")
 ca_results = client.get("igwz-8jzy")
 comm_area = pd.DataFrame.from_records(ca_results)
