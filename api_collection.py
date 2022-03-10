d8caf47fb6005e564be5411e72fea58feb275708
yifu-hou <yifu@uchicago.edu>
2022-03-09 18:51:22 -0600

    change in api collection

--- api_collection.py
+++ api_collection.py
@@ -2,13 +2,11 @@ from http.client import REQUESTED_RANGE_NOT_SATISFIABLE
 import pandas as pd
 from sodapy import Socrata
 
-##### This file replaces chicago_open_api.py
-
 
 id_dict = {
     "red light violations": "spqx-js37", "abandoned buildings": "kc9i-wq85",
     "socioeconomic indicators": "kn9c-c2s2", "hardship index": "792q-4jtu",
-    "crimes": "ijzp-q8t2", "grocery stores": "4u6w-irs9", }
+    "crimes": "ijzp-q8t2", "grocery stores": "4u6w-irs9"}
 
 
 def pull_data(dataset_id, limit):
@@ -49,6 +47,7 @@ def collect_data(regressors, limit=None):
       reg_table: (Pandas DataFrame) a dataframe merging all the
       regressors as columns, aggregated by community areas.
     '''
+    assert_valid_input(regressors)
 
     for table, values in regressors.items():
         dataset_id = id_dict[table]
@@ -68,3 +67,19 @@ def collect_data(regressors, limit=None):
         # Join Pandas DataFrames by "Community Area Name"
     
     return reg_table
+
+def assert_valid_input(d):
+    '''
+    Verify the input is from one of the designated datasets, and the
+    values conform to the standards for the regression model.
+
+    Input: d (dictionary) expected dictionary called from ui
+    '''
+
+    assert isinstance(d, dict)
+    acceptable_keys = set(["red light violations", "abandoned buildings",
+    "socioeconomic indicators", "hardship index", "crimes", "grocery stores"])
+    assert set(d.keys()).issubset(acceptable_keys)
+
+    for value in d.values():
+        assert isinstance(value, pd.DataFrame)
