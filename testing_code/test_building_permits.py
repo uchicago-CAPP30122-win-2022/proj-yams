48743f10f4c72b295d1c58b3f5135ed2432cef51
marcdloeb <marcdloeb@gmail.com>
2022-03-11 14:55:14 -0600

    testing code

---
+++ testing_code/test_building_permits.py
@@ -0,0 +1,96 @@
+import requests
+import json
+import pandas as pd
+import geopandas as gpd
+from sodapy import Socrata
+import geopy
+from geopy.extra.rate_limiter import RateLimiter
+from geopy.geocoders import Nominatim
+from geopy.geocoders import GoogleV3
+import matplotlib
+
+def get_pemits(record_limit = 1000):
+    """
+    Uses the Socrata API to access the Chciago Open Data Portal's database
+    of building permits from 2006 to the present.
+    
+    Returns:
+        perm_df: A Pandas dataframe with 
+    """
+
+    permit_types = ("PERMIT - WRECKING/DEMOLITION", "PERMIT - NEW CONSTRUCTION")
+    cols = """ID, PERMIT_, PERMIT_TYPE, APPLICATION_START_DATE,
+    STREET_NUMBER, STREET_DIRECTION, STREET_NAME, SUFFIX, WORK_DESCRIPTION,
+    REPORTED_COST, COMMUNITY_AREA, CENSUS_TRACT, XCOORDINATE, YCOORDINATE,
+    LATITUDE, LONGITUDE, LOCATION"""
+
+    client = Socrata("data.cityofchicago.org", "goD601SLndI51xcMq1KsnG6np")
+
+    permit_results = client.get("ydr8-5enu",
+                                where=f'PERMIT_TYPE in {permit_types}',
+                                select=cols, limit=record_limit)
+    perm_df = pd.DataFrame.from_records(permit_results)
+
+    col_names = {"ID": "id", "PERMIT_": "perm_num", "PERMIT_TYPE": "perm_type",
+            "APPLICATION_START_DATE": "app_date", "STREET_NUMBER": "st_num",
+            "STREET_DIRECTION": "st_dir", "STREET_NAME": "st_name",
+            "SUFFIX": "st_suffix", "WORK_DESCRIPTION": "work_desc",
+            "REPORTED_COST": "work_cost", "COMMUNITY_AREA": "comm_area",
+            "CENSUS_TRACT": "tract", "XCOORDINATE": "xcoord",
+            "YCOORDINATE": "ycoord", "LATITUDE": "lat", "LONGITUDE": "lon",
+            "LOCATION": "location"}
+    perm_df = perm_df.rename(columns=col_names)
+
+    return perm_df
+
+
+def geocode(perm_df):
+
+    # checking street address columns for na
+    street_cols = ["st_num", "st_dir", "st_name", "st_suffix"]
+    for col in street_cols: 
+        perm_df = perm_df[perm_df[col].notna()]
+
+    # creating single street address column
+    perm_df['st_addr'] = perm_df[street_cols].astype(str).agg(' '.join, axis=1) + " Chicago IL"
+
+    perm_df_nogeo = perm_df[perm_df["location"].isna()]
+    perm_df_geo = perm_df[perm_df["location"].notna()]
+
+    # geocoding
+    locator = Nominatim(user_agent="marc_cs_proj")
+    geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
+
+    for i, entry in perm_df_nogeo.iterrows():
+        print(f"\nentry number: {i}, permit number: {entry.perm_num}")
+        print(f"street address: {entry.st_addr}")
+        gcode = geocode(entry.st_addr)
+
+        if gcode:
+            perm_df_nogeo.loc[i, "lat"] = gcode.latitude
+            perm_df_nogeo.loc[i, "lon"] = gcode.longitude
+            print(f"lat: {gcode.latitude}, lon: {gcode.longitude}")
+
+        else:
+            print("not a valid address")
+
+
+    perm_df_nogeo = perm_df_nogeo[perm_df_nogeo.lat.notna()]
+    perm_df_coded = pd.concat([perm_df_nogeo, perm_df_geo], axis=0)         # len 44044
+
+    # creating a geodataframe, converting lat and lon into coords
+    geo_perm_df = gpd.GeoDataFrame(
+        perm_df_coded, geometry=gpd.points_from_xy(
+            perm_df_coded.lon, perm_df_coded.lat))
+
+    return geo_perm_df
+
+
+def save_as(geo_perm_df, name, record_limit):
+    geo_perm_df.to_file(name, driver='GeoJSON')
+
+
+perm_df = get_pemits(record_limit = 500)
+geo_perm_df = geocode(perm_df)
+name = "permits_new.geojson"
+
