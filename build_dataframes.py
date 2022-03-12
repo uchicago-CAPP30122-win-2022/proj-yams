1dfef3f110514c9ac06b5ea6068fd50e61c996d2
marcdloeb <marcdloeb@gmail.com>
2022-03-11 18:05:55 -0600

    comments done

--- build_dataframes.py
+++ build_dataframes.py
@@ -9,64 +9,63 @@ perm_df = gpd.read_file("data/permits.geojson")
 # reading in geocoded building permits
 # permits have community area numbers, but no names
 
+# working on the the comm_areas 
 demo_perm_df, build_perm_df = gca.geojoin_permits(comm_areas, perm_df)
 comm_areas = gca.merge_permits_ca(comm_areas, demo_perm_df, build_perm_df)
 census_ca = gca.get_ca_census()
 comm_areas = gca.normalize_permit_counts(comm_areas, census_ca)
-#build_year_count, demo_year_count, build_year_val = gca.permits_per_year(
-#                                    comm_areas, build_perm_df, demo_perm_df)
-
-pop = comm_areas[['community', 'area_num', 'geometry', 'tot_pop']]
-
-# new construction per year per 1000 people
-build_year_count = build_perm_df.groupby(by=["comm_area", "year"]).size().unstack()
-build_year_count = gca.per_capita(build_year_count, pop, 1000)
-
-# demolition per year per 1000 people
-demo_year_count = demo_perm_df.groupby(by=["comm_area", "year"]).size().unstack()
-demo_year_count = gca.per_capita(demo_year_count, pop, 1000)
-
-# new construction value per year
-build_year_val = build_perm_df.groupby(by=["comm_area", "year"])["work_cost"].sum().unstack()
-build_year_val = gca.per_capita(build_year_val, pop, 1)
 
 """
     finished datasets from the above functions
 
-    demo_perm_df
-
+    demo_perm_df:
         A geopandas dataframe of length 18821
         Contains the geocoded permits for "WRECKING/DEMOLITION" with filled
         in community area numbers for permits originally missing it
 
 
-    build_perm_df
-
+    build_perm_df:
         A geopandas dataframe of length 25201
         Contains the geocoded permits for "NEW CONSTRUCTION" with filled
         in community area numbers for permits originally missing it
 
 
     census_ca
-
         A pandas dataframe, with census data on population, race and
         housing for each of Chicago's 77 community areas
 
 
-    comm_areas
-
+    comm_areas:
         A pandas dataframe, with rows for each of Chicago's 77 community areas
 
         Columns of note:
-            'demo_rate'
-            'build_rate'
-            'build_per_demo'
-            'ave_build_val'
-            'change_rate'
+            'vac_rate': vacant homes divided by total housing units
+            'demo_rate': number of demolitions per capita, whole time period
+            'build_rate': number of new builds per capita, whole time period
+            'build_per_demo': ratio of new buildings to demos
+            'ave_build_val': average value of new construction
+            'change_rate': difference in build_rate and demo_rate
 
-    build_year_count, demo_year_count, 
+"""
 
+build_year_count, demo_year_count, build_year_val = gca.permits_per_year(
+    comm_areas, build_perm_df, 10000, demo_perm_df, 10000)
 
+"""
 
+    build_year_count
+
+        A pandas dataframe, with rows for each of Chicago's 77 community areas
+        with new builds per 10,000 people, per year
+    
+    demo_year_count
+
+        A pandas dataframe, with rows for each of Chicago's 77 community areas
+        with demos per 10,000 people, per year
+
+    build_year_val
+
+        A pandas dataframe, with rows for each of Chicago's 77 community areas
+        with built value per capita, per year
 
 """
--- geo_comm_areas.py
+++ geo_comm_areas.py
@@ -4,6 +4,12 @@ import pandas as pd
 
 # reading in community areas
 def get_geo_comm_areas():
+    """
+    Retrieves a geojson file of Chicago's 77 community areas
+
+    Returns a geodataframe, with a community area as each row
+    
+    """
 
     ca_url = "https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=GeoJSON"
     comm_areas = gpd.read_file(ca_url)
@@ -14,9 +20,23 @@ def get_geo_comm_areas():
     return comm_areas
 
 def geojoin_permits(comm_areas, perm_df):
+    """
+    Uses a geospatial join to assign community area numbers to
+    building permits missing that data
 
-    #comm_areas.crs # EPSG:4326
-    #perm_df.crs #EPSG:4326
+    Inputs:
+        comm_areas (geodataframe, multipolygon): Chicago's 77 community areas
+            crs: EPSG:4326
+        perm_df (geodataframe, point): ~44,500 building permits
+            previously geocoded to fill in missing lat, lon data, but
+            with some still missing community are number
+            crs: EPSG:4326
+
+    Returns:
+        demo_perm_df (geodataframe): geojoined permits for "WRECKING/DEMOLITION"
+        build_perm_df (geodataframe): geojoined permits for "NEW CONSTRUCTION"
+    
+    """
 
     # splitting date column
     perm_df[["year", "month", "day"]] = perm_df.app_date.str.split("-", expand=True)
@@ -41,8 +61,6 @@ def geojoin_permits(comm_areas, perm_df):
     perm_df_coded = perm_df_coded.astype({"comm_area": 'int16', 
                                             "work_cost": 'float'})
     # len 44024
-    # two permits mysteriously have been assigned a community area of 0
-    # even though community area numbers run from 1 to 77
     perm_df_coded = perm_df_coded[perm_df_coded.comm_area != 0]
 
     demo_perm_df = perm_df_coded[
@@ -56,6 +74,24 @@ def geojoin_permits(comm_areas, perm_df):
 
 
 def merge_permits_ca(comm_areas, demo_perm_df, build_perm_df):
+
+    """
+    Finds the total number of new buildings ("builds") and 
+    total number of new demolutions ("demos") and calculates
+    the cumulative built value ("tot_build_value") for each 
+    community area from the permit dataframes. Joins these 
+    summary statistics onto the existing comm_areas dataframe
+
+    Inputs:
+        comm_areas (geodataframe, multipolygon): Chicago's 77 community areas
+        demo_perm_df (geodataframe): geojoined permits for "WRECKING/DEMOLITION"
+        build_perm_df (geodataframe): geojoined permits for "NEW CONSTRUCTION"
+
+    Returns:
+        comm_areas, with additional columns for "builds", "demos" and
+        "tot_build_value"
+    
+    """
 
     # merging count of demos and builds per community area into comm area
     demo_count = demo_perm_df.groupby(by=["comm_area"]).size().reset_index()
@@ -80,6 +116,18 @@ def merge_permits_ca(comm_areas, demo_perm_df, build_perm_df):
 
 
 def get_ca_census():
+    """
+    Retrieves 2020 Census Supplment, with data for population, race, housing
+    for each of Chicago's 77 community areas.
+    https://datahub.cmap.illinois.gov/dataset/1d2dd970-f0a6-4736-96a1-3caeb431f5e4/resource/0916f1de-ae37-4476-bf4e-6485ba08c975
+
+    Calculates dditional columns for vacancy rate ("vac_rate"),
+    and Hispanic, White, Black and Asian pop share
+    ("hisp_per", "white_per", "black_per", "asian_per")
+    
+    Returns:
+        census_ca (dataframe): entries for each of Chicago's 77 community areas
+    """
 
     # reading in comm area census data, calculating race percent and vacancy rate
     census_url = "https://datahub.cmap.illinois.gov/dataset/1d2dd970-f0a6-4736-96a1-3caeb431f5e4/resource/0916f1de-ae37-4476-bf4e-6485ba08c975/download/Census2020SupplementCCA.csv"
@@ -98,8 +146,29 @@ def get_ca_census():
 
 def normalize_permit_counts(comm_areas, census_ca):
 
+    """
+    Joins census data to the multipolygon geodataframe for each of
+    Chicago's 77 community areas. Normalizes the counts of new builds,
+    demolitions, and total build value by population in each communtiy area
+    Calculates additional summary columns for average value of new buildings
+    and the number of new buildings per demolition for each community area
+
+    Inputs:
+        census_ca (dataframe): census data for 77 community areas
+        comm_areas (geodataframe, multipolygon): Chicago's 77 community areas
+
+    Returns:
+        comm_areas (geodataframe, multipolygon): Chicago's 77 community areas,
+            with additional columns for:
+            demolitions per capita ("demo_rate"),
+            new buildings per capita ("build_rate"), 
+            total built value per capita ("build_val_per_cap"), 
+            the average built value per new building ("ave_build_val"), 
+            the diff between build rate and the demo rate ("change_rate")
+    
+    """
+
     comm_areas = comm_areas.merge(census_ca, on="area_num")
-    #comm_areas = comm_areas.drop(columns="geog")
 
     # building and demolition rate
     comm_areas["demo_rate"] = comm_areas.demos / comm_areas.tot_pop
@@ -112,28 +181,30 @@ def normalize_permit_counts(comm_areas, census_ca):
     return comm_areas
 
 
-"""
-def permits_per_year(comm_areas, build_perm_df, demo_perm_df):
-
-    pop = comm_areas[['community', 'area_num', 'geometry', 'tot_pop']]
-
-    # new construction per year per 1000 people
-    build_year_count = build_perm_df.groupby(by=["comm_area", "year"]).size().unstack()
-    build_year_count = per_capita(build_year_count, pop, 1000)
-
-    # demolition per year per 1000 people
-    demo_year_count = demo_perm_df.groupby(by=["comm_area", "year"]).size().unstack()
-    demo_year_count = per_capita(demo_year_count, pop, 1000)
-
-    # new construction value per year
-    build_year_val = build_perm_df.groupby(by=["comm_area", "year"])["work_cost"].sum().unstack()
-    build_year_val = per_capita(build_year_val, pop, 1)
-
-    return build_year_count, demo_year_count, build_year_val
-
-"""
-
 def per_capita(df, pop, unit_size):
+
+    """
+    Helper function for permits_per_year
+
+    Limits the range of the years to 2006-2021, the time period
+    for which there is complete permit data. Joins population data
+    and multipolygon geometry. Divides value for each year by the
+    population of the corresponding community area, multiplies by
+    a specified number of persons
+
+
+    Inputs:
+        df (dataframe): a dataframe with a count or sum per community
+            area per year
+        pop (geodataframe, multipolygon): contains population count
+            geometry for each of Chicago's 77 community area
+        unit size (int): multiplies per capita rate by this population unit
+
+    Returns:
+        geodf (geodataframe, multipolygon): df with population normalized
+            counts or sums per community area per year
+    
+    """
 
     years_lst = ['2006', '2007', '2008', '2009', '2010', '2011', '2012', 
     '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']
@@ -142,4 +213,48 @@ def per_capita(df, pop, unit_size):
     df = df.merge(pop, left_on='comm_area', right_on='area_num')
     df[years_lst] = df[years_lst].div(df.tot_pop, axis=0).mul(unit_size)
 
-    return df
+    geodf = gdp.GeoDataFrame(df, crs="EPSG:4326", geometry=geometry)
+
+    return geodf
+
+
+def permits_per_year(comm_areas, build_perm_df, bsize, demo_perm_df, dsize):
+    """
+    Calculates the number of new buildings, demolitions, and new built
+    value per community area per year
+
+    Inputs:
+        comm_areas(geodataframe, multipolygon): Chicago's 77 community areas
+        build_perm_df(geodataframe): geojoined permits for "NEW CONSTRUCTION"
+        bsize(int): population unit size for new buildings count
+        demo_perm_df(geodataframe): geojoined permits for "WRECKING/DEMOLITION"
+        dsize(int): population unit size for demolitions count
+
+    Returns:
+        build_year_count(geodataframe, multipolygon): Chicago's 77 
+            community areas with new builds per 10,000 people, per year
+        demo_year_count(geodataframe, multipolygon): Chicago's 77 
+            community areas with demos per 10,000 people, per year
+        build_year_val(geodataframe, multipolygon): Chicago's 77 
+            community areas with built value per capita, per year
+
+    """
+
+    pop = comm_areas[['community', 'area_num', 'geometry', 'tot_pop']]
+
+    # new construction per year per 1000 people
+    build_year_count = build_perm_df.groupby(
+        by=["comm_area", "year"]).size().unstack()
+    build_year_count = per_capita(build_year_count, pop, bsize)
+
+    # demolition per year per 1000 people
+    demo_year_count = demo_perm_df.groupby(
+        by=["comm_area", "year"]).size().unstack()
+    demo_year_count = per_capita(demo_year_count, pop, dsize)
+
+    # new construction value per year
+    build_year_val = build_perm_df.groupby(
+        by=["comm_area", "year"])["work_cost"].sum().unstack()
+    build_year_val = per_capita(build_year_val, pop, 1)
+
+    return build_year_count, demo_year_count, build_year_val
