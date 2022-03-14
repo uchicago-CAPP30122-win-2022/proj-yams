db4b7092efe00ae8171f14d89325447ba64223f0
yifu-hou <yifu@uchicago.edu>
2022-03-14 11:47:56 -0500

    map: update

--- map/build_map.py
+++ map/build_map.py
@@ -1,4 +1,4 @@
-import pandas as pd
+
 import json
 import plotly.express as px
 import plotly.graph_objects as go
@@ -20,33 +20,12 @@ for feature in comm_areas['features']:
     feature['id'] = feature['properties']['area_numbe']
 
 app.layout = html.Div([
-    html.H1("Chicago Crime: Updated Dash", style={'text-align': 'center'}),
+
+    html.H1("Chicago Dash", style={'text-align': 'center'}),
+
     dcc.Dropdown(
         id="slct_year",
-        options=[
-            {"label": '2001', "value": '2001'},
-            {"label": '2002', "value": '2002'},
-            {"label": '2003', "value": '2003'},
-            {"label": '2003', "value": '2004'},
-            {"label": '2005', "value": '2005'},
-            {"label": '2006', "value": '2006'},
-            {"label": '2007', "value": '2007'},
-            {"label": '2008', "value": '2008'},
-            {"label": '2009', "value": '2009'},
-            {"label": '2010', "value": '2010'},
-            {"label": '2011', "value": '2011'},
-            {"label": '2012', "value": '2012'},
-            {"label": '2013', "value": '2013'},
-            {"label": '2014', "value": '2014'},
-            {"label": "2015", "value": '2015'},
-            {"label": "2016", "value": '2016'},
-            {"label": "2017", "value": '2017'},
-            {"label": "2018", "value": '2018'},
-            {"label": '2019', "value": '2019'},
-            {"label": '2020', "value": '2020'},
-            {"label": '2021', "value": '2021'},
-            {"label": '2022', "value": '2022'}
-            ],
+        options=[{'label': str(i), 'value': str(i)} for i in range(2006, 2023)],
         multi=False,
         value='2001',
         style={'width': "40%"}
@@ -84,3 +63,9 @@ def update_graph(slct_year):
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8056)
+
+
+
+
+
+
