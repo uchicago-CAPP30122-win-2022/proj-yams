18fa9ad29004b692f4ba6bfbb500af799f3b82c5
anth0nyhak1m <anthonyhakim@uchicago.edu>
2022-03-16 12:23:30 -0500

    virtual environment and packages

---
+++ install.sh
@@ -0,0 +1,13 @@
+PLATFORM=$(python3 -c 'import platform; print(platform.system())')
+
+echo -e "1. Creating new virtual environment..."
+
+python3 -m venv env 
+
+echo -e "2. Installing Requirements..."
+
+source env/bin/activate
+pip install -r requirements.txt
+
+deactivate 
+echo -e "Install is complete."
