b7e386dcf6bb12dc8869147448187c483aeb79b6
marcdloeb <marcdloeb@gmail.com>
2022-03-16 13:08:37 -0500

    blah

--- urban_dev_explorer/__main__.py
+++ urban_dev_explorer/__main__.py
@@ -1,7 +1,6 @@
 import app
 from data_processing.building_permits import get_pemits
 from  data_processing.build_dataframes import run_merge
-import ui.build_map
 import argparse
 
 
@@ -25,42 +24,5 @@ if __name__ == '__main__':
 
     # no test, so just run it
     else:
-        ui.build_map
-        
-
-
-
-
-
-"""
-import argparse
-from cdp import DataPortalCollector
-
-def main():
-    collector = DataPortalCollector()
-    limit = 200 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--limit', action="store", type=int, help='the limit of the number of libaries to receive')
-    parser.add_argument('--des', action='store_true', help='Sort the libaries by name in Z to A order (default: ascending order)')
-  
-    args = parser.parse_args()
-    
-    if args.limit: 
-        limit = args.limit
-    
-    libraries = collector.find_libraries(limit)
-
-    if args.des:
-        libraries.sort(reverse=True, key=lambda obj: obj.name)
-    else:
-        libraries.sort(key=lambda obj: obj.name)
-
-    for library in libraries:
-        print(library)
-        print("----")
-
-
-if __name__ == '__main__':
-    main() 
-
-"""
+        app.run()
+        #ui.build_map
